8dcd5d0f638289535ee2e3626ee06dcf95cd5683
Henry Sloan <henryksloan@gmail.com>
2022-03-17 16:50:57 -0400

    Add row and column run labels

--- src/main.rs
+++ src/main.rs
@@ -23,13 +23,146 @@ fn main() {
 #[derive(Component)]
 struct MainCamera;
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands
         .spawn_bundle(OrthographicCameraBundle::new_2d())
         .insert(MainCamera);
 
-    let grid = Grid::new(&mut commands, 10);
-    commands.insert_resource(grid);
+    let solution = vec![
+        vec![
+            true, true, false, false, false, false, false, true, true, false,
+        ],
+        vec![
+            false, false, false, true, false, true, false, true, true, true,
+        ],
+        vec![
+            true, true, true, true, false, false, false, true, true, true,
+        ],
+        vec![
+            false, true, false, false, false, false, false, false, true, true,
+        ],
+        vec![
+            false, false, false, false, false, false, false, false, true, true,
+        ],
+        vec![
+            false, false, false, false, false, false, true, false, true, true,
+        ],
+        vec![
+            false, false, false, false, true, false, true, true, true, true,
+        ],
+        vec![
+            true, true, true, true, true, false, true, false, false, true,
+        ],
+        vec![
+            true, true, true, true, true, true, true, false, false, false,
+        ],
+        vec![
+            true, true, true, true, true, true, true, false, false, false,
+        ],
+    ];
+
+    let puzzle = Puzzle::new(&mut commands, &asset_server, solution);
+    commands.insert_resource(puzzle);
+}
+
+fn count_runs(line: Vec<bool>) -> Vec<usize> {
+    let mut runs = Vec::new();
+    let mut curr_run = 0;
+    for cell in line {
+        if cell {
+            curr_run += 1;
+        } else if curr_run > 0 {
+            runs.push(curr_run);
+            curr_run = 0;
+        }
+    }
+
+    if curr_run > 0 {
+        runs.push(curr_run);
+    }
+
+    runs
+}
+
+struct Puzzle {
+    pub grid: Grid,
+    solution: Vec<Vec<bool>>,
+    size: usize,
+}
+
+impl Puzzle {
+    pub fn new(
+        commands: &mut Commands,
+        asset_server: &Res<AssetServer>,
+        solution: Vec<Vec<bool>>,
+    ) -> Self {
+        let size = solution.len();
+        assert!(
+            solution.iter().all(|row| row.len() == size),
+            "solution must be square"
+        );
+
+        let grid = Grid::new(commands, size);
+        let cell_size = grid.cell_size();
+
+        let row_runs: Vec<Vec<usize>> = solution
+            .iter()
+            .map(|row| count_runs(row.to_vec()))
+            .collect();
+        let col_runs: Vec<Vec<usize>> = {
+            let transpose = (0..size).map(|i| (0..size).map(|j| solution[j][i]).collect());
+            transpose.map(|row| count_runs(row)).collect()
+        };
+
+        let font = asset_server.load("fonts/FiraSans-Bold.ttf");
+        let text_style = TextStyle {
+            font,
+            font_size: 16.0,
+            color: Color::WHITE,
+        };
+        let text_alignment = TextAlignment {
+            vertical: VerticalAlign::Center,
+            horizontal: HorizontalAlign::Center,
+        };
+
+        for i in 0..size {
+            for (j, run) in row_runs[size - i - 1].iter().rev().enumerate() {
+                commands.spawn_bundle(Text2dBundle {
+                    text: Text::with_section(run.to_string(), text_style.clone(), text_alignment),
+                    transform: Transform {
+                        translation: Vec3::new(
+                            -GRID_SIZE / 2. - 15. * (j + 1) as f32,
+                            -(GRID_SIZE - cell_size) / 2. + cell_size * i as f32,
+                            10.,
+                        ),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                });
+            }
+
+            for (j, run) in col_runs[size - i - 1].iter().rev().enumerate() {
+                commands.spawn_bundle(Text2dBundle {
+                    text: Text::with_section(run.to_string(), text_style.clone(), text_alignment),
+                    transform: Transform {
+                        translation: Vec3::new(
+                            -(GRID_SIZE - cell_size) / 2. + cell_size * i as f32,
+                            GRID_SIZE / 2. + 15. * (j + 1) as f32,
+                            10.,
+                        ),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                });
+            }
+        }
+
+        Self {
+            grid,
+            solution,
+            size,
+        }
+    }
 }
 
 const GRID_SIZE: f32 = 300.;
@@ -79,7 +212,6 @@ impl Grid {
         let grid_padding = GRID_SIZE / size as f32;
         for i in 0..size - 1 {
             let sprite = Sprite {
-                // color: Color::rgb(0.0, 0.0, 0.0),
                 color: Color::rgb(0.08, 0.10, 0.62),
                 ..Default::default()
             };
@@ -90,7 +222,7 @@ impl Grid {
             commands.spawn_bundle(SpriteBundle {
                 sprite: sprite.clone(),
                 transform: Transform {
-                    translation: Vec3::new(offset - (thickness / 2.), 0., 0.),
+                    translation: Vec3::new(offset - (thickness / 2.), 0., 10.),
                     scale: Vec3::new(thickness, GRID_SIZE, 0.),
                     ..Default::default()
                 },
@@ -99,7 +231,7 @@ impl Grid {
             commands.spawn_bundle(SpriteBundle {
                 sprite: sprite.clone(),
                 transform: Transform {
-                    translation: Vec3::new(0., offset - (thickness / 2.), 0.),
+                    translation: Vec3::new(0., offset - (thickness / 2.), 10.),
                     scale: Vec3::new(GRID_SIZE, thickness, 0.),
                     ..Default::default()
                 },
@@ -150,7 +282,7 @@ impl Grid {
                     translation: Vec3::new(
                         x_pos - grid_thickness / 2.,
                         y_pos - grid_thickness / 2.,
-                        0.,
+                        1.,
                     ) + self.grid_offset(),
                     scale: cell_scale,
                     ..Default::default()
@@ -177,7 +309,6 @@ impl Grid {
     pub fn point_coords(&self, pos: Vec2) -> Option<(usize, usize)> {
         let offset = pos - self.grid_offset().truncate();
         let adjusted = offset + Vec2::new(self.cell_size() / 2., self.cell_size() / 2.);
-        println!("{:?}", adjusted);
         let row = (adjusted.x / self.cell_size()) as usize;
         let col = (adjusted.y / self.cell_size()) as usize;
         if row < self.size && col < self.size {
@@ -203,7 +334,7 @@ fn handle_mouse_clicks(
     mouse_input: Res<Input<MouseButton>>,
     windows: Res<Windows>,
     camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
-    mut grid: ResMut<Grid>,
+    mut puzzle: ResMut<Puzzle>,
 ) {
     let win = windows.get_primary().expect("no primary window");
     let (camera, camera_transform) = camera.single();
@@ -216,8 +347,8 @@ fn handle_mouse_clicks(
                 camera_transform.compute_matrix() * camera.projection_matrix.inverse();
             let world_pos = ndc_to_world.project_point3(ndc.extend(-1.0)).truncate();
 
-            if let Some((row, col)) = grid.point_coords(world_pos) {
-                grid.toggle_at(&mut commands, row, col);
+            if let Some((row, col)) = puzzle.grid.point_coords(world_pos) {
+                puzzle.grid.toggle_at(&mut commands, row, col);
             }
         }
     }
